6cc51ff6afe5ff9707c283e95f0a8a747fd21e04
Jim <71361453+jimcv@users.noreply.github.com>
2022-04-08 15:41:50 -0400

    Add C++ code to calculate

---
+++ 05-2/calculation/djb2.cpp
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::endl;
+using std::hex;
+using std::dec;
+
+unsigned int hash(unsigned int input, unsigned int oldHash)
+{
+  unsigned int hash = oldHash;
+  hash = input + hash * 33;
+  return hash;
+}
+
+unsigned int hash10(unsigned int hash32)
+{
+  unsigned int upperBits = hash32 >> 22;
+  unsigned int lowerBits = hash32 & 1023;
+  unsigned int xored = upperBits ^ lowerBits;
+  unsigned int hash10 = xored & 1023;
+  return hash10;
+}
+
+int main()
+{
+  unsigned int hash1_32 = hash(123, 5381);
+  unsigned int hash1_10 = hash10(hash1_32);
+  cout << "Hash32: " << hash1_32 << "; " << hex << hash1_32 << dec << endl;
+  cout << "Hash10: " << hash1_10 << "; " << hex << hash1_10 << dec << endl;
+  cout << endl;
+  unsigned int hash2_32 = hash(2281701376, 5381);
+  unsigned int hash2_10 = hash10(hash2_32);
+  cout << "Hash32: " << hash2_32 << "; " << hex << hash2_32 << dec << endl;
+  cout << "Hash10: " << hash2_10 << "; " << hex << hash2_10 << dec << endl;
+  return 0;
+}
+
+
